b81143c608921e2b8541e8f3daf5ba05c0a3aefe
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2021-05-13 23:28:40 +0530

    Added few solutions

--- src/main/java/com/newton/code/arrays/ClimbMinCost.java
+++ src/main/java/com/newton/code/arrays/ClimbMinCost.java
@@ -10,15 +10,24 @@ public class ClimbMinCost {
     }
 
     private static int climbCost(int[] cost) {
-      int[] minCost = new int[cost.length+1];
+        int[] minCost = new int[cost.length + 1];
 
         for (int i = 2; i < minCost.length; i++) {
-
+            int stepOne = minCost[i - 1] + cost[i - 1];
+            int stepTwo = minCost[i - 2] + cost[i - 2];
+            minCost[i] = Math.min(stepOne, stepTwo);
         }
-
-
-      return minCost[minCost.length-1];
+        return minCost[minCost.length - 1];
     }
 
+    public int minCostClimbingStairs(int[] cost) {
+        int f1 = 0, f2 = 0;
+        for (int i = cost.length - 1; i >= 0; i--) {
+            int f0 = cost[i] + Math.min(f1, f2);
+            f2 = f1;
+            f1 = f0;
+        }
+        return Math.min(f1, f2);
+    }
 
 }
