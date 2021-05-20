910ed7e87417cad363c003cff12e16bee48ef78a
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2021-05-20 19:30:17 +0530

    Added few more solutions Thu May 20 19:30:17 IST 2021

---
+++ src/main/java/com/newton/code/arrays/MonotonicArray.java
@@ -0,0 +1,24 @@
+package com.newton.code.arrays;
+
+public class MonotonicArray {
+    public static void main(String[] args) {
+        int[] nums = {7, 1, 2, 3, 4};
+
+        findMonotinic(nums);
+    }
+
+    private static boolean findMonotinic(int[] nums) {
+        boolean isIncreasing = true;
+        boolean isDecreasing = true;
+
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] < nums[i+1]) {
+                isIncreasing = false;
+            }
+            if (nums[i] > nums[i+1]) {
+                isDecreasing = false;
+            }
+        }
+        return isIncreasing || isDecreasing;
+    }
+}
---
+++ src/main/java/com/newton/code/arrays/Test.java
@@ -0,0 +1,8 @@
+package com.newton.code.arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11 - 9 : "+(Math.abs(11-9)));
+        System.out.println("9-11 : "+(Math.abs(9-11)));
+    }
+}
