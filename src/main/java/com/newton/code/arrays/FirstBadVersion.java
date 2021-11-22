348372aa699a61b04cef504550d2c5edcbeef96b
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2021-11-22 23:27:17 +0530

    Added few more solutions Mon Nov 22 23:27:17 IST 2021

---
+++ src/main/java/com/newton/code/arrays/FirstBadVersion.java
@@ -0,0 +1,31 @@
+package com.newton.code.arrays;
+
+public class FirstBadVersion {
+    public static void main(String[] args) {
+        int i = firstBadVersion(5);
+        System.out.println("Bad version found at :" + i);
+    }
+
+    public static int firstBadVersion(int n) {
+        int l = 1;
+        int r = n;
+        int m;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (isBadVersion(m)) {
+
+                r = m;
+            } else l = m + 1;
+
+        }
+        return l;
+    }
+
+    public static boolean isBadVersion(int version) {
+        if (version == 4) {
+            return true;
+        }
+        return false;
+    }
+}
+
---
+++ src/main/java/com/newton/code/arrays/SearchInsert.java
@@ -0,0 +1,20 @@
+package com.newton.code.arrays;
+
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 1;
+            if (nums[m] == target) return m;
+            if (target < nums[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
