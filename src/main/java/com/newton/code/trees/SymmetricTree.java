89c20815f6de14203c470727fc00c9afd738953f
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2022-01-06 21:58:47 +0530

    Added few more solutions Thu Jan  6 21:58:47 IST 2022

---
+++ src/main/java/com/newton/code/trees/SymmetricTree.java
@@ -0,0 +1,32 @@
+package com.newton.code.trees;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        node.left = new Node(2);
+        node.right = new Node(2);
+
+        node.left.left = new Node(3);
+        node.left.right = new Node(4);
+
+        node.right.left = new Node(3);
+        node.right.right = new Node(4);
+
+        isSymmetric(node);
+    }
+
+    public static boolean isSymmetric(Node root) {
+        return compare(root.left, root.right);
+    }
+
+    public static boolean compare(Node p, Node q) {
+
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        if (p.data != q.data) return false;
+        return compare(p.left, q.right) && compare(p.right, q.left);
+    }
+
+
+}
