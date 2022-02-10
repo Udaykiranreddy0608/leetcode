b10394bd92a13deebb3df45d0915a7797a994d0a
Uday Tippireddy <Uday.Tippireddy@paysafe.com>
2022-02-10 12:13:15 +0530

    Added few more solutions Thu, Feb 10, 2022 12:13:15

--- src/main/java/com/newton/code/arrays/HeightChecker.java
+++ src/main/java/com/newton/code/arrays/HeightChecker.java
@@ -2,25 +2,39 @@ package com.newton.code.arrays;
 
 public class HeightChecker {
   public static void main(String[] args) {
-    int[] arr = {0, 1, 2};
+    int[] arr = {3, 4, 5, 0};
     heightChecker(arr);
 
   }
 
   public static int heightChecker(int[] heights) {
     int cnt = 0;
-    int max = heights[0];
 
-    for (int i = 1; i < heights.length; i++) {
 
-      if (heights[i] >= max) {
-        max = heights[i];
-      } else {
+    int[] sortHeight = new int[heights.length];
+    for (int i = 0; i < heights.length; i++) {
+      sortHeight[i] = heights[i];
+    }
+
+    for (int i = 0; i < sortHeight.length; i++) {
+      for (int j = i + 1; j < sortHeight.length; j++) {
+
+        if (sortHeight[i] > sortHeight[j]) {
+          int temp = sortHeight[i];
+          sortHeight[i] = sortHeight[j];
+          sortHeight[j] = temp;
+        }
+      }
+    }
+
+    for (int i = 0; i < heights.length; i++) {
+      if (heights[i] != sortHeight[i]) {
         cnt++;
       }
-
     }
+
     return cnt;
   }
+
 
 }
