2aadc8eb58d09d63d996b29af560addaeb778e3e
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2022-03-25 15:40:42 +0530

    Added few more solutions Fri Mar 25 15:40:42 IST 2022

---
+++ src/main/java/com/newton/code/dp/GridTraveller.java
@@ -0,0 +1,23 @@
+package com.newton.code.dp;
+
+import java.util.HashMap;
+
+public class GridTraveller {
+    public static void main(String[] args) {
+
+        System.out.println("Number of paths : " + uniquePaths(100, 100));
+    }
+
+    private static HashMap<String, Integer> map = new HashMap<>();
+
+    public static int uniquePaths(int m, int n) {
+        String key = m + "_" + n;
+        if (map.containsKey(key)) return map.get(key);
+        if (m == 1 && n == 1) return 1;
+        if (m == 0 || n == 0) return 0;
+        map.put(key, uniquePaths(m - 1, n) + uniquePaths(m, n - 1));
+        return map.get(key);
+    }
+
+
+}
