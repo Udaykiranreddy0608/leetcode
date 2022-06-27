fa4790d88823efb7c91b296fb841c7d6e482c15b
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2022-06-27 23:29:24 +0530

    Added few more solutions Mon Jun 27 23:29:24 IST 2022

--- src/main/java/com/newton/code/trees/BalancedTree.java
+++ src/main/java/com/newton/code/trees/BalancedTree.java
@@ -5,24 +5,29 @@ public class BalancedTree {
 
         TreeNode treeNode = new TreeNode(1);
         treeNode.left = new TreeNode(2);
+        // treeNode.left.right = new TreeNode(2);
+        // treeNode.left.left = new TreeNode(2);
+
         treeNode.right = new TreeNode(3);
         treeNode.right.left = new TreeNode(4);
         treeNode.right.right = new TreeNode(5);
+        treeNode.right.left.left = new TreeNode(4);
+        treeNode.right.left.right = new TreeNode(5);
         System.out.printf("isBalanced : " + isBalanced(treeNode));
 
     }
 
     public static boolean isBalanced(TreeNode root) {
-
-        if (root != null) {
-            if (root.left != null && root.right == null) {
-                return false;
-            }
-            if (root.left == null && root.right != null) {
-                return false;
-            }
-        }
-
+        if (root == null) return true;
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+        if (Math.abs(lheight - rheight) > 1) return false;
+        System.out.println("Max depth :" + root.val + "\t L height" + lheight + "\t R height" + rheight);
         return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public static int height(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(height(node.left), height(node.right)) + 1;
     }
 }
