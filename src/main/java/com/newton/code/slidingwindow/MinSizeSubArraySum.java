984fe36dea4d942b6cda158364024f60f21072f5
Udaykiran Reddy Tippireddy <udaykiranreddy0608@gmail.com>
2022-06-30 22:27:28 +0530

    Added few more solutions Thu Jun 30 22:27:28 IST 2022

---
+++ src/main/java/com/newton/code/slidingwindow/MinSizeSubArraySum.java
@@ -0,0 +1,30 @@
+package com.newton.code.slidingwindow;
+
+/**
+ * Minimum Size Subarray Sum
+ * <p>
+ * Given an array of positive integers nums and a positive integer target,
+ * return the minimal length of a contiguous subarray
+ * [numsl, numsl+1, ..., numsr-1, numsr] of which the sum is greater than or
+ * equal to target. If there is no such subarray, return 0 instead.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: target = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+ * Example 2:
+ * <p>
+ * Input: target = 4, nums = [1,4,4]
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+ * Output: 0
+ */
+public class MinSizeSubArraySum {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello");
+    }
+}
